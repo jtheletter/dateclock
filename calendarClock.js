74ecac2c85b77253e0b846c2de9f5a48b55a4a21
JP <49571924+jtheletter@users.noreply.github.com>
2018-01-29 19:11:59 -0800

    Change datetime update logic for background bug.

--- calendarClock.js
+++ calendarClock.js
@@ -1,91 +1,17 @@
 // Lunisolar Calendar-Clock. (c) JP 2009 (concept). (c) 2018 (code, in progress).
 
-// 2hr Fix bug where minute+ updates late after screen in background.
 // 2hr Toggle DST/ST.
 // 2hr "Month/Day/Hour/Minute/Second" central title & outer glow on numer/hand on mouseover/touch.
 // 1hr Set user-select-none on most elements.
 // 1hr Update favicon, OG image.
-// 1hr Element.requestFullscreen().
 // 1hr Cookie settings.
 // 8hr Add lunar calculations.
-// 3hr Info screen?
+// 2hr Audit performance.
 // 1hr Troubleshoot older iOS.
+// 3hr Info screen?
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    function getDaysInMonth(date) {
-        return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-    }
-    function rotate(el, degs) {
-        el.style.transform = `rotate(${degs}deg)`;
-    }
-    function updateMonth (datetime = new Date()) {
-        let month = datetime.getMonth() + 1; // Add one month for natural counting.
-        let day = datetime.getDate();
-        let daysInMonth = getDaysInMonth(datetime);
-        let rotation = (month - 1 + (day - 1) / daysInMonth) / 12 * 360; // Subtract one month (and day) for zero indexing.
-        rotate(els.handMonth, rotation);
-        els.digitalMonth.textContent = month < 10 ? `0${month}` : month;
-    }
-    function updateDay (datetime = new Date()) {
-        let day = datetime.getDate();
-        let hour = datetime.getHours();
-        let daysInMonth = getDaysInMonth(datetime);
-        let rotation = (day - 1 + hour / 24) / daysInMonth * 360; // Subtract one day for zero indexing.
-        rotate(els.handDay, rotation);
-        els.digitalDay.textContent = day < 10 ? `0${day}` : day;
-        if (hour === 0) { // Update month once per day.
-            updateMonth(datetime);
-        }
-    }
-    function updateHour (datetime = new Date()) {
-        let hour = datetime.getHours();
-        let minute = datetime.getMinutes();
-        let rotation = (hour + minute / 60) / 24 * 360;
-        rotate(els.handHour, rotation);
-        els.digitalHour.textContent = hour < 10 ? `0${hour}` : hour;
-        if (minute === 0) { // Update day once per hour.
-            updateDay(datetime);
-        }
-    }
-    function updateMinute (datetime = new Date()) {
-        let minute = datetime.getMinutes();
-        let second = datetime.getSeconds();
-        let rotation = (minute + second / 60) / 60 * 360;
-        rotate(els.handMinute, rotation);
-        els.digitalMinute.textContent = minute < 10 ? `0${minute}` : minute;
-        if (second === 0) { // Update hour once per minute.
-            updateHour(datetime);
-        }
-    }
-    function updateSecond (datetime = new Date()) {
-        let second = datetime.getSeconds();
-        let millisecond = datetime.getMilliseconds();
-        let rotation = (second + millisecond / 1000) / 60 * 360;
-        rotate(els.handSecond, rotation);
-        els.digitalSecond.textContent = second < 10 ? `0${second}` : second;
-        if (millisecond === 0) { // Update minute once per second.
-            updateMinute(datetime);
-        }
-    }
-    function updateOffset (datetime = new Date()) {
-        let offset = new Date().getTimezoneOffset() / 60;
-        if (offset > 0) {
-            els.digitalOffset.textContent = `\u002d${offset}`; // hyphen-minus sign
-        } else if (offset < 0) {
-            els.digitalOffset.textContent = `\u002b${offset}`; // plus sign
-        } else {
-            els.digitalOffset.textContent = `\u00b1${offset}`; // plus-minus sign
-        }
-    }
-    function updateAll (datetime = new Date()) {
-        updateMonth(datetime);
-        updateDay(datetime);
-        updateHour(datetime);
-        updateMinute(datetime);
-        updateSecond(datetime);
-        updateOffset(datetime);
-    }
     function drawPipMonth () {
         let i, el, degs;
         for (i = 1; i <= 12; i++) {
@@ -128,7 +54,79 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    function rotate(el, degs) {
+        el.style.transform = `rotate(${degs}deg)`;
+    }
+    function getDaysInMonth(date) {
+        return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    }
+
+    function setOffset (datetime = new Date()) {
+        let offset = new Date().getTimezoneOffset() / 60;
+        if (offset > 0) {
+            els.digitalOffset.textContent = `\u002d${offset}`; // hyphen-minus sign
+        } else if (offset < 0) {
+            els.digitalOffset.textContent = `\u002b${offset}`; // plus sign
+        } else {
+            els.digitalOffset.textContent = `\u00b1${offset}`; // plus-minus sign
+        }
+    }
+    function setMonth (datetime = new Date()) {
+        let month = datetime.getMonth() + 1; // Add one month for natural counting.
+        let day = datetime.getDate();
+        let daysInMonth = getDaysInMonth(datetime);
+        let degs = (month - 1 + (day - 1) / daysInMonth) / 12 * 360; // Subtract one month (and day) for zero indexing.
+        rotate(els.handMonth, degs);
+        els.digitalMonth.textContent = month < 10 ? `0${month}` : month;
+    }
+    function setDay (datetime = new Date()) {
+        let day = datetime.getDate();
+        let hour = datetime.getHours();
+        let daysInMonth = getDaysInMonth(datetime);
+        let degs = (day - 1 + hour / 24) / daysInMonth * 360; // Subtract one day for zero indexing.
+        rotate(els.handDay, degs);
+        els.digitalDay.textContent = day < 10 ? `0${day}` : day;
+        prevSetDay = datetime;
+    }
+    function setHour (datetime = new Date()) {
+        let hour = datetime.getHours();
+        let minute = datetime.getMinutes();
+        let degs = (hour + minute / 60) / 24 * 360;
+        rotate(els.handHour, degs);
+        els.digitalHour.textContent = hour < 10 ? `0${hour}` : hour;
+        if (minute === 0 || datetime - prevSetDay > 1000 * 60 * 60) { // Update day, month, & offset each hour.
+            setDay(datetime);
+            setMonth(datetime);
+            setOffset(datetime);
+        }
+        prevSetHour = datetime;
+    }
+    function setMinute (datetime = new Date()) {
+        let minute = datetime.getMinutes();
+        let second = datetime.getSeconds();
+        let degs = (minute + second / 60) / 60 * 360;
+        rotate(els.handMinute, degs);
+        els.digitalMinute.textContent = minute < 10 ? `0${minute}` : minute;
+        if (second === 0 || datetime - prevSetHour > 1000 * 60) { // Update hour each minute.
+            setHour(datetime);
+        }
+        prevSetMinute = datetime;
+    }
+    function setTime (datetime = new Date()) {
+        let second = datetime.getSeconds();
+        let millisecond = datetime.getMilliseconds();
+        let degs = (second + millisecond / 1000) / 60 * 360;
+        rotate(els.handSecond, degs);
+        els.digitalSecond.textContent = second < 10 ? `0${second}` : second;
+        if (millisecond === 0 || datetime - prevSetMinute > 1000) { // Update minute each second.
+            setMinute(datetime);
+        }
+    }
+
     let els = {};
+    let prevSetDay = 0;
+    let prevSetHour = 0;
+    let prevSetMinute = 0;
 
     els.pipMonth = document.getElementById('pip-month');
     els.pipDay = document.getElementById('pip-day');
@@ -154,8 +152,7 @@ document.addEventListener('DOMContentLoaded', function () {
     drawPipHour();
     drawPipMinute();
 
-    // updateAll(new Date(1970, 3 - 1, 25, 9, 35, 42.5)); // "Factory Display"
-    updateAll();
-    setInterval(updateSecond, 40); // Arbitray rate that looks good enough onscreen.
+    // setTime(new Date(1970, 3 - 1, 25, 9, 35, 42.5)); // "Factory Display"
+    setInterval(setTime, 40); // Arbitray rate that looks good enough onscreen.
 
 });
